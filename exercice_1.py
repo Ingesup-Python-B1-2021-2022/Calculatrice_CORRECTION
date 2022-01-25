2b3c035d10ed4a807bef8fd967c1a5475b9073df
Allta <51991304+Allta@users.noreply.github.com>
2022-01-25 15:33:31 +0100

    Delete exercice_1.py

--- exercice_1.py
+++
@@ -1,8 +0,0 @@
-# Créer un objet de type chaîne de caractères
-chaine = 
-# Créer un objet de type nombre entier
-nombre_entier = 
-# Créer objet de type nombre décimal
-nombre_decimal = 
-# Créer un objet de type booléen
-boolean = 
