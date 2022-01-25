110dc65b736f5084512465bee185e0114ea1a235
Allta <51991304+Allta@users.noreply.github.com>
2022-01-25 15:33:16 +0100

    Delete exerice_2.py

--- exerice_2.py
+++
@@ -1,4 +0,0 @@
-print('Bonjour tout le monde !")
-print('Je m'appelle Patrick.')
-print("Je dis souvent "la vie est un long fleuve tranquille" quand on s'appelle Patrick.")
-print(Oui, je suis très philosophe.)
